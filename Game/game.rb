113d7151cb4fee6e2059d91ebf42673b4ebdba1e
Desiree Mendes <desireefrost@live.ca>
2022-01-11 18:07:51 -0500

    Define start and end of game

--- Game/game.rb
+++ Game/game.rb
@@ -3,19 +3,30 @@ require './questions'
 
 class Game
   def initialize
-    @player1 = Player.new('Desiree', 8)
-    @player2 = Player.new('Opponent', 8)
+    @player1 = Player.new('Desiree')
+    @player2 = Player.new('Opponent')
     @current_player = @player1
-  end
-
-  def turn
-  end
-
-  def score
   end
 
   def start game
     puts "Game has begun"
     puts "/n"
+
+    puts "Player One, whats your name?"
+    @player1 = Player.new(gets.chomp)
+
+    puts "Player Two, what's your name?"
+    @player2 = Player.new(gets.chomp)
+
+    puts "Time to solve some math!"
+    puts "\n"
+    @current_player = @player1
+
+    game_play while (@player1.lives > 0 && @player2.lives > 0)
+    end_game
+  end
+
+  def turn
+  end
 
 end
